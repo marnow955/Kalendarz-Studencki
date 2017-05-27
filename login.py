f3da12c25d328d2cefef41995749ed90b6b7c6ab
marnow955 <marnow955@gmail.com>
2017-05-27 11:50:54 +0200

    v1.0

---
+++ login.py
@@ -0,0 +1,13 @@
+#!C:/Program Files (x86)/Python27/python
+import cgi
+print """ Content-type: text/html\n\n
+ <!DOCTYPE html>
+ <html lang="en">
+    <head>
+      <title>Server-side scripting</title>
+    </head>
+    <body>
+      <h1>This is my first Server-side scripting in Python!</h1>
+    </body>
+ </html>
+"""
